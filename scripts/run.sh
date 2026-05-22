8dd0bf364cbffe353decc59dfb6efd57f163dede
Sanka Peeris <sankapeeris360@gmail.com>
2026-05-22 07:13:11 +0000

    Add run.sh

---
+++ scripts/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for appliance in "WashingMachine" "Dishwasher" "Kettle" "Microwave" "Fridge"; do
+    echo "Running experiment for $appliance..."
+    uv run -m scripts.run_one_expe \
+        --dataset "UKDALE" \
+        --sampling_rate "10s" \
+        --appliance "$appliance" \
+        --window_size 256 \
+        --name_model BERT4NILM \
+        --seed 2
+    echo "Done: $appliance"
+done
