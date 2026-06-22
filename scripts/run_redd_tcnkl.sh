70b513e073e799b3f4fa03545f8ad3a3b9a56528
Sanka Peeris <sankapeeris360@gmail.com>
2026-06-22 08:24:51 +0000

    Add tcn redd runs

--- scripts/run_redd_tcnkl.sh
+++ scripts/run_redd_tcnkl.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-for appliance in "WashingMachine" "Dishwasher" "Microwave" "Fridge"; do
+for appliance in "WasherDryer"; do
     for window_size in 128 256 512; do
         for seed in 0 1 2; do
             echo "Running experiment for $appliance ws=$window_size seed=$seed model=TCN_KL..."
             uv run -m scripts.run_one_expe \
                 --dataset "REDD" \
-                --sampling_rate "1min" \
+                --sampling_rate "10s" \
                 --appliance "$appliance" \
                 --window_size "$window_size" \
                 --name_model "TCN_KL" \
